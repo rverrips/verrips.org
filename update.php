bdddb8b3225d7b4415fdc8c2b4d1d6baf85080ed
www-data <lucy.verrips.org>
2013-07-31 03:10:43 +0400

    updated log file format

--- update.php
+++ update.php
@@ -1,1 +1,4 @@
-<?php `git pull`;  
+<?php 
+`echo "---------------------------" >> /www/verrips.org/update.txt`;
+`date >> /www/verrips.org/update.txt`;
+`git pull >> /www/verrips.org/update.txt`;  
