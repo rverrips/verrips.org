5d299734d4f7442d93f4cefdc2cf7cbe020ff1ae
Roy Verrips <roy@verrips.org>
2013-07-31 02:13:46 +0300

    Test new update log

--- update.php
+++ update.php
@@ -1,4 +1,4 @@
 <?php 
-`echo "---------------------------" >> /www/verrips.org/update.txt`;
+`echo "-------------------------" >> /www/verrips.org/update.txt`;
 `date >> /www/verrips.org/update.txt`;
 `git pull >> /www/verrips.org/update.txt`;  
